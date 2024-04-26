c20c9a09427d7408b964348a7ef687a8430235ef
AlexLinshits <ipod2505@gmail.com>
2024-04-25 23:53:04 -0700

    started cy

---
+++ Check Yourself/script.js
@@ -0,0 +1,17 @@
+(function(){
+'use strict'
+
+function exercise (name, days, frequency){
+    this.name 
+}
+
+var exercise = {
+    name: ["running","calisthenics", "weightlifting", "running"],
+    days: ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday","Rest"],
+    time: ["30mins","1 hour","2 Hours"]
+}
+
+exercise.name = [0];
+exercise.days = [1,3,4];
+exercise.time = [2]
+}());
